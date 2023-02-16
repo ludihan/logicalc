21297a96f450b60e41529349d172d1d5d8330f0e
ludihan <luccad.han@gmail.com>
2023-02-16 14:00:19 -0300

    Dumb syntax checking

--- src/expr.rs
+++ src/expr.rs
@@ -1,15 +1,47 @@
+use colored::*;
+
 pub fn eval(expr: &str) {
     syntax_check(expr);
 }
 
 fn syntax_check(expr: &str) {
-
-    let expr_string: String = expr.split_whitespace().collect();
-    let expr_vec: Vec<char> = expr_string.chars().collect();
+    //let expr_string: String = expr.split_whitespace().collect();
+    //let expr_vec: Vec<char> = expr_string.chars().collect();
     
-    let previous_element: char = ' ';
-    for elements in expr_vec.iter() {
-        
+    let expr_vec: Vec<char> = {
+        let expr_string: String = expr.split_whitespace().collect();
+        expr_string.chars().collect()
+    };
+
+    //let previous_element: char = ' ';
+
+    for index in 0..expr_vec.len() {
+        if expr_vec[index].is_alphabetic() {
+            continue;
+        } else if expr_vec[index] == '(' || expr_vec[index] == ')' {
+            continue;
+        }else if 
+            expr_vec[index] == '~' || 
+            expr_vec[index] == '^' ||
+            expr_vec[index] == 'v' ||
+            expr_vec[index] == '>' ||
+            expr_vec[index] == '=' 
+        {
+            continue;
+        } else {
+            //println
+            error_print(&expr_vec, index, "Invalid symbol");
+        }
     }
-    println!("{:?}", expr_vec);
+}
+
+fn error_print(expr_vec: &Vec<char>, index: usize, error: &str) {
+    println!("");
+    for element in expr_vec.iter() {
+        print!("{}",element.to_string().red());
+    }
+    println!("");
+    println!("{arrow:>index$}", arrow = "^".red(), index = index + 1);
+    println!("{arrow:>index$}", arrow = "|".red(), index = index + 1);
+    println!("{error:>index$}", error = error.red(), index = index + error.len());
 }
