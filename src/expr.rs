9b2bc75c37ec3c6a57b426bba2c8a9443c5183ee
ludihan <luccad.han@gmail.com>
2023-02-25 20:56:32 -0300

    Incomplete syntax checking

--- src/expr.rs
+++ src/expr.rs
@@ -1,30 +1,41 @@
 use colored::*;
 
 pub fn eval(expr: &str) {
-    syntax_check(expr);
-}
-
-fn syntax_check(expr: &str) {
     let expr_vec: Vec<char> = {
         let expr_string: String = expr.split_whitespace().collect();
         expr_string.chars().collect()
     };
+    semantic_check(&expr_vec);
+}
 
+fn semantic_check(expr_vec: &Vec<char>) -> bool {
+    println!("{:?}", expr_vec);
     for index in 0..expr_vec.len() {
-        if expr_vec[index].is_alphabetic()
-            || expr_vec[index] == '('
-            || expr_vec[index] == ')'
-            || expr_vec[index] == '~'
-            || expr_vec[index] == '^'
-            || expr_vec[index] == 'v'
-            || expr_vec[index] == '>'
-            || expr_vec[index] == '='
-        {
-            continue;
-        } else {
-            error_print(&expr_vec, index, "Invalid symbol");
+        match expr_vec[index] {
+            char if char.is_alphabetic() => {}
+
+            '~' => {}
+
+            '^' => {}
+
+            'v' => {}
+
+            '>' => {}
+
+            '=' => {}
+
+            '(' => match semantic_check(&expr_vec[index + 1..].to_owned()) {
+                true => continue,
+                false => error_print(expr_vec, index, "Missing ending parenthesis"),
+            },
+
+            ')' => {
+                return true;
+            }
+            _ => error_print(expr_vec, index, "Invalid symbol"),
         }
     }
+    false
 }
 
 fn error_print(expr_vec: &[char], index: usize, error: &str) {
