da6dbec8ee936839a514a81be3b1e9b20df1bd0a
ludihan <luccad.han@gmail.com>
2023-02-27 15:15:16 -0300

    Complete syntax checking

---
+++ src/logic.rs
@@ -0,0 +1,216 @@
+use colored::*;
+
+pub fn from(expr: &str) {
+    let logic_vec: Vec<char> = {
+        let logic_string: String = expr.split_whitespace().collect();
+        logic_string.chars().collect()
+    };
+
+    syntax_check(&logic_vec);
+}
+
+fn syntax_check(logic_vec: &Vec<char>) {
+    let mut opened_parenthesis = 0;
+
+    // first element
+    match logic_vec[0] {
+        '(' => opened_parenthesis += 1,
+        char if char.is_ascii_uppercase() => (),
+        _ => {
+            error_print(&logic_vec, 0, "syntax error");
+            return;
+        }
+    }
+
+    //last element
+    match logic_vec[logic_vec.len() - 1] {
+        ')' => (),
+        char if char.is_ascii_uppercase() => (),
+        _ => {
+            error_print(&logic_vec, logic_vec.len() - 1, "syntax error");
+            return;
+        }
+    }
+
+    // all other elements
+    // ~ ^ v > = ) ( A
+    for index in 0..logic_vec.len() - 1 {
+        match logic_vec[index] {
+            char if char.is_ascii_uppercase() => match logic_vec[index + 1] {
+                '^' | 'v' | '>' | '=' => continue,
+                ')' => {
+                    opened_parenthesis -= 1;
+                    continue;
+                }
+                _ => {
+                    error_print(logic_vec, index, "syntax error");
+                    return;
+                }
+            },
+
+            '~' => match logic_vec[index + 1] {
+                next_char if next_char.is_ascii_uppercase() => continue,
+                '(' => {
+                    opened_parenthesis += 1;
+                    continue;
+                }
+                ')' => {
+                    opened_parenthesis -= 1;
+                    continue;
+                }
+                '~' => continue,
+                _ => {
+                    error_print(logic_vec, index, "syntax error");
+                    return;
+                }
+            },
+
+            '^' => match logic_vec[index + 1] {
+                next_char if next_char.is_ascii_uppercase() => continue,
+                '(' => {
+                    opened_parenthesis += 1;
+                    continue;
+                }
+                ')' => {
+                    opened_parenthesis -= 1;
+                    continue;
+                }
+                '~' => continue,
+                _ => {
+                    error_print(logic_vec, index, "syntax error");
+                    return;
+                }
+            },
+
+            'v' => match logic_vec[index + 1] {
+                next_char if next_char.is_ascii_uppercase() => continue,
+                '(' => {
+                    opened_parenthesis += 1;
+                    continue;
+                }
+                ')' => {
+                    opened_parenthesis -= 1;
+                    continue;
+                }
+                '~' => continue,
+                _ => {
+                    error_print(logic_vec, index, "syntax error");
+                    return;
+                }
+            },
+
+            '>' => match logic_vec[index + 1] {
+                next_char if next_char.is_ascii_uppercase() => continue,
+                '(' => {
+                    opened_parenthesis += 1;
+                    continue;
+                }
+                ')' => {
+                    opened_parenthesis -= 1;
+                    continue;
+                }
+                '~' => continue,
+                _ => {
+                    error_print(logic_vec, index, "syntax error");
+                    return;
+                }
+            },
+
+            '=' => match logic_vec[index + 1] {
+                next_char if next_char.is_ascii_uppercase() => continue,
+                '(' => {
+                    opened_parenthesis += 1;
+                    continue;
+                }
+                ')' => {
+                    opened_parenthesis -= 1;
+                    continue;
+                }
+                '~' => continue,
+                _ => {
+                    error_print(logic_vec, index, "syntax error");
+                    return;
+                }
+            },
+
+            '(' => match logic_vec[index + 1] {
+                next_char if next_char.is_ascii_uppercase() => {
+                    continue;
+                }
+                '~' | '^' | 'v' | '>' | '=' => {
+                    continue;
+                }
+                '(' => {
+                    opened_parenthesis += 1;
+                    continue;
+                }
+                _ => {
+                    error_print(logic_vec, index, "syntax error");
+                    return;
+                }
+            },
+
+            ')' => match logic_vec[index + 1] {
+                '^' | 'v' | '>' | '=' => {
+                    continue;
+                }
+                ')' => {
+                    opened_parenthesis -= 1;
+                    continue;
+                }
+                _ => {
+                    error_print(logic_vec, index, "syntax error");
+                    return;
+                }
+            },
+
+            _ => {
+                error_print(logic_vec, index, "illegal symbol");
+                return;
+            }
+        }
+    }
+
+    //println!("{opened_parenthesis}");
+
+    if opened_parenthesis > 0 {
+        error_print(logic_vec, logic_vec.len() - 1, "missing parenthesis");
+        return;
+    }
+
+    logic_slicer(logic_vec);
+}
+
+fn logic_slicer(logic_vec: &Vec<char>) {
+    let mut logic_vecs: Vec<Vec<char>> = Vec::new();
+    for index in 0..logic_vec.len() {
+
+        match logic_vec[index] {
+            '(' => {
+
+            }
+            ')' => {
+                
+            }
+            _ => ()
+        }
+    }
+}
+
+fn logic_eval(logic_vec: &Vec<char>) {}
+
+fn error_print(logic_vec: &[char], index: usize, error: &str) {
+    println!();
+    for element in logic_vec.iter() {
+        print!("{}", element.to_string().red());
+    }
+    println!();
+    println!("{arrow:>index$}", arrow = "^".red(), index = index + 1);
+    println!("{arrow:>index$}", arrow = "|".red(), index = index + 1);
+    println!(
+        "{error:>index$}",
+        error = error.red(),
+        index = index + error.len()
+    );
+    println!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+//use std::io;
+//use std::io::Write;
 use colored::*;
-use std::io;
-use std::io::Write;
-
-mod expr;
-
-fn main() {
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
+mod logic;
+fn main() -> Result<()> {
     println!(
         "{}",
         "logicalc (2023-2023) [github.com/ludihan/logicalc]"
@@ -12,9 +12,45 @@ fn main() {
             .bold()
             .italic()
     );
-    println!("{}", "operations: [~][^][v][>][=]".green());
+    println!("{}", "operations: [~][^][v][>][=][(][)]".green());
+    println!("{}", "use UPPERCASE ascii characters for variables".green());
     println!("{}", "type \"exit\" to close the program\n".green());
 
+    let mut rl = DefaultEditor::new()?;
+    #[cfg(feature = "with-file-history")]
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                if line.trim().eq_ignore_ascii_case("exit") {
+                    break;
+                }
+                logic::from(&line);
+                //println!("Line: {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C pressed. bye");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D pressed. bye");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    #[cfg(feature = "with-file-history")]
+    rl.save_history("history.txt");
+    Ok(())
+
+    /*
     loop {
         print!("{}", "> ".bold());
         std::io::stdout().flush().unwrap();
@@ -31,6 +67,7 @@ fn main() {
             break;
         }
 
-        expr::eval(&input);
+        logic::from(&input);
     }
+    */
 }
